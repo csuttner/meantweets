6ed0a4d588bf85c29a407a58c3b3aadc06650a4e
Clay Suttner <csuttner@gmail.com>
2018-09-29 16:59:50 -0700

    adjusted attenuating function

--- meantweet.py
+++ meantweet.py
@@ -2,43 +2,42 @@ import math
 from cleaner import tweetparse
 from mtclasses import eword
 
-tweets = tweetparse('obama.csv')	
+tweets = tweetparse('Obama.csv')	
 
 uwords = {}	
 wordsoup = []
 numwords = 0
-numsens = 0
 
 # collect properties of every word with eword class
 for tweet in tweets:
 	t_len = sum(len(t) for t in tweet)
-	tw_indx = 0
+	t_inx = 0
 	for sentence in tweet:
 		s_len = len(sentence)
-		numsens += 1
-		for wo_c, word in enumerate(sentence):
-			wordsoup.append(eword(word,s_len,wo_c,t_len,tw_indx))
+		for count, word in enumerate(sentence):
+			wordsoup.append(eword(word,s_len,count,t_len,t_inx))
 			numwords += 1
-			tw_indx += 1
+			t_inx += 1
 			if word not in uwords.keys():
 				uwords[word] = 1
 			else:
 				uwords[word] += 1
 		
-wavg_int = int(round(float(numwords) / len(tweets)))
+avw_int = int(round(numwords / len(tweets)))
+avw_fl = float(avw_int)
 
 # each element in outarr is an array of all of the body's ewords who's normalized indexes 
 # fall into the range, sorted by highest to lowest total adjusted score
 
 outarr = []
 
-for n in range(wavg_int):
+for n in range(avw_int):
 	inarr = []
 	inwords = []
 	# loop through the array of every word in the body's eword objects
 	for o in wordsoup:
 		# if the normalized index of the word falls within the range of n to n+1
-		if o.tnormindx < (n+1)/float(wavg_int) and o.tnormindx >= n/(float(wavg_int)):
+		if o.tnormindx < (n+1)/avw_fl and o.tnormindx >= n/avw_fl:
 			if o.word not in inwords:
 				inwords.append(o.word)
 				inarr.append(o)
@@ -51,11 +50,11 @@ for n in range(wavg_int):
 	for r in inarr:
 		r.tval = r.tval ** (1/math.sqrt(uwords[r.word]))
 	
-	# sort
+	# sort by attentuated score
 	inarr.sort(key = lambda x: x.tval, reverse = True)
 	outarr.append(inarr)
 
-# printing the words with highest sval in respective slot, & runner upsx
+# printing the words with highest sval in respective slot, & runner ups
 for q in outarr:
 	print q[0].word, q[0].tval, q[1].word, q[1].tval, q[2].word, q[2].tval
 
