6c10ad6515e468d87cbfb32dc37085b8140fb919
Clay Suttner <csuttner@gmail.com>
2018-10-03 19:34:23 -0700

    made code runnable

--- meantweet.py
+++ meantweet.py
@@ -1,9 +1,8 @@
 import sys
 import math
 from cleaner import tweetparse
-import matplotlib.pyplot as plt
 
-tweets = tweetparse('obama.csv')
+tweets = tweetparse(sys.argv[-1])
 uwords = {}	
 wordsoup = []
 numwords = 0
@@ -23,27 +22,15 @@ outarr1 = []
 for spot in range(avwords_int):
 	indic = {}
 	for werd in wordsoup:
-		# if the normalized index of the word falls within the range of n to n+1
 		if werd[1] < (spot+1)/avwords_fl and werd[1] >= spot/avwords_fl:
 			indic[werd[0]] = indic.get(werd[0],0) + werd[2] 
-	# once bin full, attenuate scores of common words
 	for itm in indic:
 		indic[itm] = indic[itm] ** (1/math.sqrt(uwords[itm]))
 	# sort by attentuated score
-	outarr.append(sorted(indic, key=indic.__getitem__, reverse = True)) 
-	outarr1.append(sorted(indic, reverse = True))
+	outarr.append(sorted(indic, key=indic.__getitem__, reverse = True))
+
 # printing the words with highest sval in respective slot, & runner ups
 printout = ''
-x = []
-y = []
-for inx, q in enumerate(outarr):
-	# printout += q[0] + ' '
-	x.append(outarr1[inx])
-	y.append(uwords[q[0]])
-# print printout
-
-print x
-print y
-
-plt.scatter(x,y)
-plt.show()
+for q in outarr:
+	printout += q[0] + ' '
+print printout
