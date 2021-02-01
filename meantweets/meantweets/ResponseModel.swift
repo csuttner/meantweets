2025791da576cae343f284a5bd2fc4e27b8647a7
Clay Suttner <csuttner@gmail.com>
2021-02-01 12:24:29 -0800

    builds response data model and renames file to ResponseModel.swift

---
+++ meantweets/meantweets/ResponseModel.swift
@@ -0,0 +1,22 @@
+//
+//  TweetModel.swift
+//  meantweets
+//
+//  Created by Clay Suttner on 8/17/20.
+//  Copyright © 2020 Fairchild-Suttner. All rights reserved.
+//
+
+import Foundation
+
+struct MTResponse: Decodable {
+    let handle: String
+    let tweet_count: Int
+    let distinct_words: Int
+    let words: [Word]
+}
+
+struct Word: Decodable {
+    let value: String
+    let count: Int
+    let score: Int
+}
